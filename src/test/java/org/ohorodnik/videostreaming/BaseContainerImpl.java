c0df31b2cfdf6ca5032c349cd87aee7c1671629c
alexone <asogorodnik1986@gmail.com>
2025-01-14 15:32:37 +0200

    Add two metadata integration tests;

--- src/test/java/org/ohorodnik/videostreaming/BaseContainerImpl.java
+++ src/test/java/org/ohorodnik/videostreaming/BaseContainerImpl.java
@@ -1,14 +1,23 @@
 package org.ohorodnik.videostreaming;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.spring.api.DBRider;
+import org.apache.commons.io.FileUtils;
 import org.ohorodnik.videostreaming.utils.TestConfigurationToCountAllQueries;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import static com.github.database.rider.core.api.configuration.Orthography.LOWERCASE;
 
@@ -21,6 +30,8 @@ import static com.github.database.rider.core.api.configuration.Orthography.LOWER
 public class BaseContainerImpl {
 
     private static final PostgreSQLContainer<?> container;
+    @Autowired
+    protected ObjectMapper objectMapper;
 
     static {
         container = new PostgreSQLContainer<>("postgres:latest")
@@ -36,5 +47,14 @@ public class BaseContainerImpl {
         registry.add("spring.datasource.url", container::getJdbcUrl);
         registry.add("spring.datasource.password", container::getPassword);
         registry.add("spring.datasource.username", container::getUsername);
+    }
+
+    protected String getResponseAsString(String jsonPath) {
+        URL resource = getClass().getClassLoader().getResource(jsonPath);
+        try {
+            return FileUtils.readFileToString(new File(resource.toURI()), StandardCharsets.UTF_8);
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException("Unable to find file: " + jsonPath, e);
+        }
     }
 }
--- src/test/java/org/ohorodnik/videostreaming/web/controller/MetadataControllerITest.java
+++ src/test/java/org/ohorodnik/videostreaming/web/controller/MetadataControllerITest.java
@@ -1,11 +1,67 @@
 package org.ohorodnik.videostreaming.web.controller;
 
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import org.junit.jupiter.api.Test;
 import org.ohorodnik.videostreaming.BaseContainerImpl;
+import org.ohorodnik.videostreaming.dto.AddUpdateMetadataDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 public class MetadataControllerITest extends BaseContainerImpl {
+
+    private static final String ADDED_METADATA_DATASET = "datasets/metadata/added-metadata-dataset.json";
+    private static final String FINDALL_METADATA_DATASET = "datasets/metadata/findall-metadata-dataset.json";
+    private static final String VIDEO_FOR_ADDED_METADATA_DATASET = "datasets/video/video-for-added-metadata-dataset.json";
+    private static final String FINDALL_VIDEO_DATASET = "datasets/video/findall-video-dataset.json";
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    @DataSet(value = VIDEO_FOR_ADDED_METADATA_DATASET, skipCleaningFor = "flyway_schema_history")
+    @ExpectedDataSet(value = {ADDED_METADATA_DATASET, VIDEO_FOR_ADDED_METADATA_DATASET})
+    public void whenAddMetadata_thenAddedMetadataReturnedAndOkStatusReceived() throws Exception {
+        AddUpdateMetadataDto metadataDto = createAddDtoWithAllData();
+
+        mockMvc.perform(post("/api/v1/metadata/add/065b1c00-22ca-43e6-bd1a-3a571b394041")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(metadataDto)))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json(getResponseAsString("responses/metadata/added-metadata.json")));
+    }
+
+    @Test
+    @DataSet(value = {FINDALL_METADATA_DATASET, FINDALL_VIDEO_DATASET}, skipCleaningFor = "flyway_schema_history")
+    public void testFindAll() throws Exception {
+
+        mockMvc.perform(get("/api/v1/metadata")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(content()
+                        .json(getResponseAsString("responses/metadata/find-all-metadata.json")));
+    }
+
+    private AddUpdateMetadataDto createAddDtoWithAllData() {
+        return AddUpdateMetadataDto.builder()
+                .title("testTitle")
+                .synopsis("test synopsis")
+                .director("testDirector")
+                .actors("actorFirst, actorSecond")
+                .yearOfRelease("2019-01-01")
+                .genre("testGenre")
+                .runningTime("PT5M")
+                .build();
+    }
 }
